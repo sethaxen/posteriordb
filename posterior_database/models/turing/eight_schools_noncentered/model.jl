1b61262abadadd17a85d32eb21fb9e74e362c452
Seth Axen <seth@sethaxen.com>
2022-11-02 15:19:44 +0100

    Update model

--- posterior_database/models/turing/eight_schools_noncentered/model.jl
+++ posterior_database/models/turing/eight_schools_noncentered/model.jl
@@ -1,19 +1,30 @@
 module EightSchoolsNoncentered
 
-using LinearAlgebra, Turing
+using FillArrays
+using LinearAlgebra
+using Turing
 
-@model function model(
-    data;
-    J=data["J"],  # number of schools
-    y=data["y"],  # estimated treatment
-    sigma=data["sigma"]  # std of estimated effect
-)
-    theta_trans ~ filldist(Normal(0, 1), J)  # transformation of theta
+struct EightSchoolsNoncenteredModel{T<:AbstractVector{<:Real}}
+    """number of schools"""
+    J::Int
+    """variance of estimated effect"""
+    sigma2::T
+end
+
+@model function (model::EightSchoolsNoncenteredModel)()
+    theta_trans ~ MvNormal(Zeros(model.J), I)  # transformation of theta
     mu ~ Normal(0, 5)  # hyper-parameter of mean, non-informative prior
     tau ~ Cauchy(0, 5)  # hyper-parameter of sd
     theta = theta_trans .* tau .+ mu  # original theta
-    y .~ Normal.(theta, sigma)
+    y ~ MvNormal(theta, Diagonal(model.sigma2))  # estimated treatment
     return (; theta=theta)
+end
+
+function model(data::AbstractDict{String})
+    J = data["J"]
+    sigma2 = data["sigma"].^2
+    y = data["y"]
+    return EightSchoolsModel(J, sigma2) | (; y=y)
 end
 
 end # module
